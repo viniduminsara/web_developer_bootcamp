9aa7bb53ca04b4df6e3520670347de12139f431e
viniduminsara <viniduminsara@gmail.com>
2023-07-25 22:42:41 +0530

    add lesson

---
+++ javascript_lessons/object/app.js
@@ -0,0 +1,23 @@
+//object creation
+
+const person = {
+    name   : 'Vinidu',
+    age    : 18,
+    gender : 'Male',
+}
+
+console.log(person);
+
+//access the object valuse
+
+//method 01
+
+let nameofPerson = person['name'];
+
+console.log(nameofPerson);
+
+//method 2
+
+let ageOfPerson = person.age;
+
+console.log(ageOfPerson);
