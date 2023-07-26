4194fa97bb3afcf8258b5398cbae5f991beeb1ed
viniduminsara <viniduminsara@gmail.com>
2023-07-26 19:10:49 +0530

    add lessons

---
+++ javascript_lessons/loops/app.js
@@ -0,0 +1,11 @@
+for(let i = 0; i<=10 ; i++){
+    console.log(i);
+}
+
+//iterating array by for loop
+
+const animal = ['Dog','Cat','Lion','Tiger'];
+
+for(let i = 0; i<=animal.length ; i++){
+    console.log(animal[i]);
+}
