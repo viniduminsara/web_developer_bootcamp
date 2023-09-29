d2b12226ccedd005e0d84717404572a0971879f9
viniduminsara <viniduminsara@gmail.com>
2023-09-29 11:33:33 +0530

    add lesson

---
+++ javascript_lessons/promises/promiseCreation/app.js
@@ -0,0 +1,28 @@
+function colorChange(color, delay){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay);
+    })
+}
+
+// colorChange('red',2000)
+//     .then((response) => {
+//         console.log(response);
+//         colorChange('orange', 2000)
+//         .then((response) => {
+//             console.log(response);
+//             colorChange('yellow', 2000)
+//             .then((response) => {
+//                 console.log(response);
+//             })
+//         })
+//     })
+
+colorChange('red', 2000)
+    .then(() => colorChange('orange', 2000))
+    .then(() => colorChange('yellow', 2000))
+    .then(() => {colorChange('green', 2000)})
+    
+    
