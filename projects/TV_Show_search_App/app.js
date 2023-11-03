7c65cd5632c24fc3bed3fc5bbda831b2c232ba8c
viniduminsara <viniduminsara@gmail.com>
2023-11-03 14:51:07 +0530

    few changes

--- projects/TV_Show_search_App/app.js
+++ projects/TV_Show_search_App/app.js
@@ -1,23 +1,34 @@
-const form = document.querySelector('form');
+const button = document.querySelector('button');
+const searchText = document.querySelector('input');
+const row = document.querySelector('.row:nth-child(2)');
 
-form.addEventListener('submit', async function(e){
-    e.preventDefault();
-    const searchTerm = form.elements.query.value;
+button.addEventListener('click', async function(){
+    const searchTerm = searchText.value;
     const config = {
         params: { 
             q: searchTerm
         }
     };
     const res = await axios.get('https://api.tvmaze.com/search/shows', config);
-    showImages(res.data);
+    showResults(res.data);
 })
 
-function showImages(shows){
+function showResults(shows){
+    let htmlString = '';
+
     shows.forEach(result => {
         if(result.show.image){
-            const newImg = document.createElement('img');
-            newImg.src = result.show.image.medium;
-            document.body.append(newImg);
+            htmlString += `
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 card">
+                    <img src="${result.show.image.medium}" class="card-img-top">
+                    <div class="card-body">
+                        <b>${result.show.name}</b><br>
+                        <p>Ratings : ${result.show.rating.average || 0}</p>
+                        <a href="${result.show._links.self.href}" class="btn btn-primary" >Watch TV Show</a>
+                    </div>
+                </div>
+            `;
+            row.innerHTML = htmlString;
         }
     });
 }
