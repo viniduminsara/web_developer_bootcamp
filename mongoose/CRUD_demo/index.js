a9d4e7a50c0a68d502e7185dd3bbbf62f8457534
viniduminsara <viniduminsara@gmail.com>
2023-11-30 19:26:51 +0530

    complete add product

--- mongoose/CRUD_demo/index.js
+++ mongoose/CRUD_demo/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const fs = require('fs');
 const Product = require('./models/product');
 const port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/CRUD_demo')
     .then(() => {
@@ -19,6 +22,18 @@ app.listen(port, () => {
     console.log('Listening on port 3000');
 });
 
+// Configure multer storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({ storage: storage });
+
 app.get('/home', (req, res) => {
     res.render('home/index', { currentPage: 'Home' });
 });
@@ -28,11 +43,47 @@ app.get('/products', async(req, res) => {
     res.render('products/index', {products, currentPage: 'Products'});
 });
 
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { currentPage: 'NewProduct' });
+});
+
+app.get('/products/:id/edit', async(req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', {})
+})
+
 app.get('/products/:id', async(req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/view', {product, currentPage: product.name});
-})
+});
+
+app.post('/products', upload.single('image'), async(req, res) => {
+    // Access the uploaded file information through req.file
+    const { name, price, qty, category } = req.body;
+    const imagePath = req.file ? req.file.path : null;
+
+    // Read the image file as binary data
+    let imageBuffer = undefined;
+    if(imagePath){
+        imageBuffer = fs.readFileSync(imagePath);
+    }
+
+    const newProduct = new Product({
+        name: name,
+        price: price,
+        qty: qty,
+        category: category,
+        image: {
+            data: imageBuffer,
+            contentType: 'image/jpeg'
+        }
+    });
+
+    await newProduct.save();
+    res.redirect(`/products/${newProduct.id}`);
+});
 
 
 
