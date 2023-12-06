be61b166594548e9608abefb7735822dc5c8d659
viniduminsara <viniduminsara@gmail.com>
2023-12-06 16:15:37 +0530

    complete express error handling

--- express/error_handling/index.js
+++ express/error_handling/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const AppError = require('./AppError');
 
 app.use(morgan('common'));
 
@@ -13,18 +14,34 @@ const verifyPassword = (req, res, next) => {
     if(password === '12345'){
         return next();
     }
-    res.send('Password incorrect');
-}
+    //use builtin error handler
+    // throw new Error('Password incorrect');
 
+    //use custom error handling class
+    throw new AppError('Password required', 401);
+    
+}
 
 app.get('/secret', verifyPassword, (req, res) => {
     res.send('My name is Vinidu Minsara.');
 });
 
-app.get('/dogs', (req, res) => {
-    res.send(`Requst Date : ${req.requestTime}`);
+app.get('/error', (req, res) => {
+    chicken.fly();
 });
 
-app.use((req, res, next) => {
-    res.status(404).send('Not Found!');
+//custom error handler
+// app.use((err, req, res, next) => {
+//     console.log('*******************************************');
+//     console.log('*******************ERROR*******************');
+//     console.log('*******************************************');
+//     res.status(500).send('Error');
+
+//     //if want call express built-in error handlers
+//     next(err);
+// });
+
+app.use((err, req, res, next) => {
+    const {status = 500, message = 'Something went wrong'} = err;
+    res.status(status).send(message);
 });
