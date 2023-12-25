c6c821a0b37b5b33e74d9dff8d526b530ab44db6
viniduminsara <viniduminsara@gmail.com>
2023-12-25 20:02:50 +0530

    add lesson

---
+++ express/auth/bcrypt/index.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcrypt');
+
+const hashpassword = async(password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    console.log(salt);
+    console.log(hash);
+} 
+
+const login = async(password, hashpw) => {
+    const res = await bcrypt.compare(password, hashpw);
+    console.log(res  ? 'Password matched' : 'Password not matched');
+}
+
+hashpassword('12345');
+
+login('12345', '$2b$10$gVmv9MnaiXI6MsoPnm6ytuuNa80GAEQjR2PqVLWz9BhaPpBxEkcUO');
+git status
