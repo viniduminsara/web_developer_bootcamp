cb79fed6908c65ace84e803a82cdda1211be8ea0
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-01-10 21:53:03 +0530

    add react lessons

---
+++ react/first-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Chicken from './Chicken'
+
+function App() {
+  return <div>
+    <Chicken/>
+    <Chicken/>
+    <Chicken/>
+  </div>
+}
+
+export default App
---
+++ react/first-react-app/src/Chicken.jsx
@@ -0,0 +1,7 @@
+import './Chicken.css'
+
+function Chicken(){
+    return <p className="Chicken">Chicken</p>
+}
+
+export default Chicken;
